8f5ecb5c4f09668109f6867fd81d9fa6656cc413
Gabriel-Schiestl <gabriel.schiestl25@hotmail.com>
2024-08-23 22:00:55 -0300

    fix

--- src/IA.ts
+++ src/IA.ts
@@ -1,20 +1,18 @@
 import { Board, Player } from "./tabuleiro";
-import { combinations } from "./checkWinner";
 
 interface IIA {
     board: Board;
     combinations: number[][];
-    nextMove(board: Board): number;
+    nextMove(): number;
 }
 
 export class IA implements IIA {
-
     board: Board;
     readonly combinations = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],
         [0, 3, 6], [1, 4, 7], [2, 5, 8],
         [0, 4, 8], [2, 4, 6]
-    ]
+    ];
 
     constructor(board: Board) {
         this.board = board;
@@ -24,8 +22,9 @@ export class IA implements IIA {
         return (
             this.checkWinningMove('O') ||
             this.checkBlockingMove() ||
-            this.chooseRandomMove()
-        )
+            this.chooseRandomMove() ||
+            -1
+        );
     }
 
     private checkWinningMove(player: Player): number | null {
@@ -33,16 +32,13 @@ export class IA implements IIA {
             if (this.board.array[a] === player && this.board.array[b] === player && this.board.array[c] === null) {
                 return c;
             }
-
             if (this.board.array[a] === player && this.board.array[c] === player && this.board.array[b] === null) {
                 return b;
             }
-
             if (this.board.array[b] === player && this.board.array[c] === player && this.board.array[a] === null) {
                 return a;
             }
         }
-
         return null;
     }
 
@@ -50,18 +46,16 @@ export class IA implements IIA {
         return this.checkWinningMove('X');
     }
 
-    private chooseRandomMove(): number {
-        for (const [a, b, c] of combinations) {
-            if (this.board[a] === 'O' && this.board[b] === null && this.board[c] === null) {
-                return c;
-            }
+    private chooseRandomMove(): number | null {
+        const emptyCells = this.board.array
+            .map((cell, index) => cell === null ? index : null)
+            .filter(index => index !== null) as number[];
 
-            if (this.board[c] === 'O' && this.board[a] === null && this.board[b] === null) {
-                return a;
-            }
-
-            return b;
+        if (emptyCells.length > 0) {
+            const randomIndex = Math.floor(Math.random() * emptyCells.length);
+            return emptyCells[randomIndex];
         }
-    }
 
+        return null;
+    }
 }
